2d72a61be82fc946164b5749d0d0e23464aed767
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-03-21 14:49:30 +0300

    Update СhangeText.cs

--- СhangeText.cs
+++ СhangeText.cs
@@ -1,27 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 
 public class СhangeText : MonoBehaviour
 {
-    [SerializeField] private Text _replacingText;
-    [SerializeField] private Text _additionText;
-    [SerializeField] private Text _hackingText;
+    [SerializeField] private Text _text;
     [SerializeField] private float _delay;
     [SerializeField] private float _duration;
 
     private void Start()
     {
-        string replacingText = "Я заменил этот текст";
-        string additionText = "\nЭто дополнение к тексту";
-        string hackingText = "Я взломал этот текст";
+        Sequence sequence = DOTween.Sequence().SetDelay(_delay);
 
         ScrambleMode scrambleMode = ScrambleMode.All;
 
         bool richTextEnabled = true;
 
-        _replacingText.DOText(replacingText, _duration).SetDelay(_delay);
-        _additionText.DOText(additionText, _duration).SetRelative().SetDelay(_delay);
-        _hackingText.DOText(hackingText, _duration, richTextEnabled, scrambleMode).SetDelay(_delay);
+        string replacingText = "Я заменил этот текст";
+        string additionText = "\nЭто дополнение к тексту";
+        string hackingText = "Я взломал этот текст";
+
+        sequence.Append(_text.DOText(replacingText, _duration));
+        sequence.Append(_text.DOText(additionText, _duration).SetRelative());
+        sequence.Append(_text.DOText(hackingText, _duration, richTextEnabled, scrambleMode));
     }
 }
