c88d90f9d87b3c45769c309726f37c4805150929
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-03-21 13:12:54 +0300

    Update RotateTo.cs

--- RotateTo.cs
+++ RotateTo.cs
@@ -4,11 +4,11 @@ using DG.Tweening;
 public class RotateTo : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotate;
-    [SerializeField] private float _durationRotate;
+    [SerializeField] private float _duration;
     [SerializeField] private float _delay;
 
     private void Start()
     {
-        transform.DORotate(_rotate, _durationRotate).SetDelay(_delay);
+        transform.DORotate(_rotate, _duration).SetDelay(_delay);
     }
 }
